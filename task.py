5f8345b967e7082f1e5001c65b9f062ebc8a12f4
miku26 <147724820+miku26@users.noreply.github.com>
2024-07-11 10:46:15 +0500

    Update task.py

--- task.py
+++ task.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
